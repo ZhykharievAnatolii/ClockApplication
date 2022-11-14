8f01af34205d89a3a7f8fdc5481bfcc70e7a81f5
ZhykharievAnatolii <nekijt87@gmail.com>
2022-11-14 13:35:16 +0200

    add files
    add first version of application

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict"
+/**
+ *
+ * @type {Date} date
+ */
+const now=new Date();
+function updateTime(date) {
+    const hours=document.querySelector('.hours');
+    const minutes=document.querySelector('.minutes');
+    const seconds=document.querySelector('.seconds');
+
+    hours.innerText=date.getHours().toString().padStart(2, '0');
+    minutes.innerText=date.getMinutes().toString().padStart(2, '0');
+    seconds.innerText=date.getSeconds().toString().padStart(2, '0');
+
+}
+updateTime(now);
+// setInterval(()=>{
+// now.setSeconds(now.getSeconds()+1);
+//     updateTime(now);
+// },1000)
+const intervalId=setInterval(()=>{
+    now.setSeconds(now.getSeconds()+1);
+    updateTime(now);
+},1000);
