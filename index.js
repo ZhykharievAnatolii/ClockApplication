9eabbb97cc2a3cea6bceeeac8429ce5a13d23516
ZhykharievAnatolii <nekijt87@gmail.com>
2022-11-15 20:27:01 +0200

    upd js & css
    add new params for calendar

--- index.js
+++ index.js
@@ -32,3 +32,41 @@ const intervalId=setInterval(()=>{
 //     return notification;
 //
 // };
+function createCalendar(elem, year, month) {
+    let mon = month - 1; // месяцы в JS идут от 0 до 11, а не от 1 до 12
+    let d = new Date(year, mon);
+    let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
+    // пробелы для первого ряда
+    // с понедельника до первого дня месяца
+    // * * * 1  2  3  4
+    for (let i = 0; i < getDay(d); i++) {
+        table += '<td></td>';
+    }
+    // <td> ячейки календаря с датами
+    while (d.getMonth() == mon) {
+        table += '<td>' + d.getDate() + '</td>';
+
+        if (getDay(d) % 7 == 6) { // вс, последний день - перевод строки
+            table += '</tr><tr>';
+        }
+        d.setDate(d.getDate() + 1);
+    }
+    // добить таблицу пустыми ячейками, если нужно
+    // 29 30 31 * * * *
+    if (getDay(d) != 0) {
+        for (let i = getDay(d); i < 7; i++) {
+            table += '<td></td>';
+        }
+    }
+    // закрыть таблицу
+    table += '</tr></table>';
+
+    elem.innerHTML = table;
+}
+function getDay(date) { // получить номер дня недели, от 0 (пн) до 6 (вс)
+    let day = date.getDay();
+    if (day == 0) day = 7; // сделать воскресенье (0) последним днем
+    return day - 1;
+}
+
+createCalendar(calendar, 2022, 11);
