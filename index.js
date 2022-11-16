a34b4fa7cc8183022c1fcbd40f1e8b18bde367f2
ZhykharievAnatolii <nekijt87@gmail.com>
2022-11-16 21:29:59 +0200

    upd js
    add new about button

--- index.js
+++ index.js
@@ -70,3 +70,31 @@ function getDay(date) { // получить номер дня недели, от
 }
 
 createCalendar(calendar, 2022, 11);
+
+
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+const names = ['Bohdan', 'Stepan', 'Yura', 'Julia', 'Olena'];
+
+const renderItem = (text) => {
+    const item = document.createElement('li');
+    const button = document.createElement('button');
+    button.innerText = 'X';
+    item.innerText = text;
+    item.append(button);
+    button.onclick = () => {
+        item.remove();
+    };
+    item.onmousedown = () => {
+        item.classList.add('red');
+    };
+
+    item.onmouseup = () => {
+        item.classList.remove('red');
+    }
+
+
+    list.append(item);
+};
+
+names.forEach(renderItem);
